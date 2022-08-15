1aa806e8f7a287291e09bb63595c931db8bb69cb
nikolas1370 <leow1357@gmail.com>
2022-08-15 16:28:56 +0300

    Add files via upload

--- wscs.js
+++ wscs.js
@@ -181,7 +181,6 @@ class wscs// WebSocketControlerServer
                 {       
                     data = Buffer.from(new Int8Array(data));                        
                     let headerStr =  strToUtf8Array(JSON.stringify(typeof(header) === "object" ? header : "{}"))
-                    console.log(data.byteLength)
                     var buf = Buffer.alloc(packetMetaSize + 1 + data.byteLength + 10 +  headerStr.byteLength);
                     let strbl = strToUtf8Array(String(data.byteLength))
                     for (let i = packetMetaSize + 1, j = 0; i < buf.byteLength &&  j < strbl.byteLength; i++, j++) 
@@ -294,57 +293,3 @@ class Connection
         this.setEventClose = setEventClose;
     }
 }
-
-
-
-
-   
-
-
-
-/*
-в браузері wscc
-npm install wscs
-
-////
-const fs = require("fs")
-const path = require("path")
- var ssl = { 
-        key: fs.readFileSync(path.join(__dirname, "cert", "ca.key")),
-        cert: fs.readFileSync(path.join(__dirname, "cert", "ca.crt")) 
-    }    
-
-
-const wscs = require("wscs");
-// ssl не обов'язковий 
-// connect визивається при новом зєднанні і передається Connection
-const wsServer =  new wscs( 3010, ssl , (connection)  =>
-{
-    connection.send("event", undefined)
-    connection.send("event", null)
-    connection.send("event", 12)
-    connection.send("event", "12")
-    connection.send("event", {})
-    connection.send("event", Buffer | ArrayBuffer | DataView, {descriphen  : "клієнт получе цей обєк разом із Buffer", descriphen2 : "не обов'язково"})
-
-
-    
-    
-    
-    // підписуєшся на входяче повідомленя
-    // виклик функції з тією самою подією в другий раз перевизначить попередню функцію
-    connection.on("incomingEvent", (message, header) =>
-    { // клієнт може надіслати бінарні дані і вдобавок header
-        console.log(message, header);
-
-        connection.removeOn("incomingEvent")// тепер в цю функцію не прийде повідомлення
-    });
-
-    connection.setEventClose = () => console.log("з'єднання закрите")
-});
-
-
-
-
-
-*/
